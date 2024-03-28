53b2febcaa219bf6c9c9d021555c16d7d562c157
Dzakiyatur Rosyidah <zakia.rosi@gmail.com>
2024-03-28 11:43:22 +0700

    CREATE BERHASIL

--- pages/content/antrianoperasi/data-antrian-operasi.vue
+++ pages/content/antrianoperasi/data-antrian-operasi.vue
@@ -1,110 +1,209 @@
-<!--https://vuetifyjs.com/en/components/data-tables/data-and-display/#filterable--> 
 <template>
-      <v-card
-      title="Data Antrian Operasi"
-      flat
-    >
-      <v-card-title class="d-flex align-center pe-2">
-        <v-icon icon="mdi-video-input-component"></v-icon> &nbsp;
-        Find a Graphics Card
-  
-        <v-spacer></v-spacer>
-  
-        <v-text-field
-          v-model="search"
-          density="compact"
-          label="Search"
-          prepend-inner-icon="mdi-magnify"
-          variant="solo-filled"
-          flat
-          hide-details
-          single-line
-        ></v-text-field>
-      </v-card-title>
-  
-      <v-divider></v-divider>
-      <v-data-table v-model:search="search" :items="items">
-        <template v-slot:header.stock>
-          <div class="text-end">Stock</div>
-        </template>
-  
-        <template v-slot:item.image="{ item }">
-          <v-card class="my-2" elevation="2" rounded>
-            <v-img
-              :src="`https://cdn.vuetifyjs.com/docs/images/graphics/gpus/${item.image}`"
-              height="64"
-              cover
-            ></v-img>
-          </v-card>
-        </template>
-  
-        <template v-slot:item.rating="{ item }">
-          <v-rating
-            :model-value="item.rating"
-            color="orange-darken-2"
-            density="compact"
-            size="small"
-            readonly
-          ></v-rating>
-        </template>
-  
-        <template v-slot:item.stock="{ item }">
-          <div class="text-end">
-            <v-chip
-              :color="item.stock ? 'green' : 'red'"
-              :text="item.stock ? 'In stock' : 'Out of stock'"
-              class="text-uppercase"
-              size="small"
-              label
-            ></v-chip>
-          </div>
-        </template>
-      </v-data-table>
-    </v-card>
-  </template>
-  <script>
-    export default {
-      data () {
-        return {
-          search: '',
-          items: [
-            {
-              name: 'Nebula GTX 3080',
-              image: '1.png',
-              price: 699.99,
-              rating: 5,
-              stock: true,
-            },
-            {
-              name: 'Galaxy RTX 3080',
-              image: '2.png',
-              price: 799.99,
-              rating: 4,
-              stock: false,
-            },
-            {
-              name: 'Orion RX 6800 XT',
-              image: '3.png',
-              price: 649.99,
-              rating: 3,
-              stock: true,
-            },
-            {
-              name: 'Vortex RTX 3090',
-              image: '4.png',
-              price: 1499.99,
-              rating: 4,
-              stock: true,
-            },
-            {
-              name: 'Cosmos GTX 1660 Super',
-              image: '5.png',
-              price: 299.99,
-              rating: 4,
-              stock: false,
-            },
-          ],
-        }
-      },
-    }
-  </script>
+  <v-card class="mx-auto">
+    <v-data-table :headers="headers" :items="dataUser" :items-per-page="8">
+      <!--judul tabel dan button new item di pojok kanan-->
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Data User SIM RS</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="600px">
+            <template v-slot:activator="{ props }">
+              <v-btn class="mb-2" color="primary" dark v-bind="props">
+                TAMBAH DATA
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" md="4" sm="6">
+                      <v-text-field
+                        v-model="editedItem.nip"
+                        label="NIP"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" sm="6">
+                      <v-text-field
+                        v-model="editedItem.nama_pegawai"
+                        label="Nama Pegawai"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" sm="6">
+                      <v-text-field
+                        v-model="editedItem.rolename"
+                        label="NIP / NIPTTK"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" sm="6">
+                      <v-text-field
+                        v-model="editedItem.nama_unit"
+                        label="Departemen"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4" sm="6">
+                      <v-text-field
+                        v-model="editedItem.nama_grupunit"
+                        label="Aktif"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="close">
+                  BATAL
+                </v-btn>
+                <v-btn color="blue-darken-1" variant="text" @click="save">
+                  SIMPAN
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5"
+                >Are you sure you want to delete this item?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
+                  >Cancel</v-btn
+                >
+                <v-btn
+                  color="blue-darken-1"
+                  variant="text"
+                  @click="deleteItemConfirm"
+                  >OK</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <!--end of v-slot top-->
+
+      <!--actions kolom edit dan delete -->
+      <template v-slot:item.actions="{ item }">
+        <v-icon class="me-2" size="small" @click="detailItem(item)">
+          mdi-account-details
+        </v-icon>
+        <v-icon class="me-2" size="small" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+      <!--end of actions-->
+    </v-data-table>
+  </v-card>
+</template>
+
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const userDetail = ref();
+const dataUser = ref();
+const headers = [
+  { title: "NIP", value: "nip", sortable: true },
+  { title: "Nama Pegawai", value: "nama_pegawai", sortable: true },
+  { title: "Role", value: "rolename", sortable: true },
+  { title: "Nama Unit", value: "nama_unit", sortable: true },
+  { title: "Nama Grup Unit", value: "nama_grupunit", sortable: true },
+  { title: "Actions", key: "actions", sortable: false },
+];
+
+//tambahan
+
+const dialog = ref(false);
+const dialogDelete = ref(false);
+const editedIndex = ref(-1);
+const editedItem = ref({
+  name: "",
+  calories: 0,
+  fat: 0,
+  carbs: 0,
+  protein: 0,
+});
+const defaultItem = ref({
+  name: "",
+  calories: 0,
+  fat: 0,
+  carbs: 0,
+  protein: 0,
+});
+const formTitle = computed(() => {
+  return editedIndex.value === -1 ? "New Item" : "Edit Item";
+});
+//end of tambahan
+
+const { data: userSimRS } = useFetch("/api/user/userdata");
+const { data: userDetailData } = useFetch("/api/user/userdata"); // Assuming userDetail endpoint
+
+//tambahan
+function detailItem(item) {
+  editedIndex.value = dataUser.value.indexOf(item);
+  editedItem.value = Object.assign({}, item);
+  dialog.value = true;
+}
+
+function editItem(item) {
+  editedIndex.value = dataUser.value.indexOf(item);
+  editedItem.value = Object.assign({}, item);
+  dialog.value = true;
+}
+function deleteItem(item) {
+  editedIndex.value = deserts.value.indexOf(item);
+  editedItem.value = Object.assign({}, item);
+  dialog.value = true;
+}
+function deleteItemConfirm() {
+  deserts.value.splice(editedIndex.value, 1);
+  close();
+}
+function close() {
+  dialog.value = false;
+  nextTick(() => {
+    editedItem.value = Object.assign({}, defaultItem.value);
+    editedIndex.value = -1;
+  });
+}
+function closeDelete() {
+  dialogDelete.value = false;
+  nextTick(() => {
+    editedItem.value = Object.assign({}, defaultItem.value);
+    editedIndex.value = -1;
+  });
+}
+function save() {
+  if (editedIndex.value > -1) {
+    Object.assign(dataUser.value[editedIndex.value], editedItem.value);
+  } else {
+    dataUser.value.push(editedItem.value);
+  }
+  close();
+}
+watch(dialog, (val) => {
+  val || close();
+});
+watch(dialogDelete, (val) => {
+  val || closeDelete();
+});
+//end of tambahan
+
+onMounted(async () => {
+  // Await both fetches to ensure data is available before use
+  await userSimRS.value;
+  await userDetailData.value;
+
+  dataUser.value = userSimRS.value; // Assuming userSimRS has user data
+  userDetail.value = userDetailData.value; // Assuming userDetailData has detail data
+});
+</script>
